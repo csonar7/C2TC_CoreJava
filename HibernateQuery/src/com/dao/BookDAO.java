4c8f66fa6a6c33cf2eac7501e3cbf360b5e5eb02
LENOVO <LENOVO@DESKTOP-197U0KE>
2022-05-20 18:02:40 +0530

    commit first

---
+++ HibernateQuery/src/com/dao/BookDAO.java
@@ -0,0 +1,17 @@
+package com.dao;
+import java.util.List;
+
+import com.entities.Book;
+
+
+public interface BookDAO {
+	void commitTranscation();
+	void beginTranscation();
+	void addBook(Book book);
+	Book getBookById(int id);
+	List<Book> getBookByTitle(String title);
+	Long getBookCount();
+	List<Book> getAuthorBooks(String author);
+	List<Book> getAllBooks();
+	List<Book> getBooksInPriceRange(double low, double high);
+}
